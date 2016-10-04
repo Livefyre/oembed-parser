084a6fd6b38983fbbbb5531ea89a9e392115ff7d
Devon Govett <devongovett@gmail.com>
2016-10-04 16:00:07 -0700

    Ignore content inside noscript tags

--- src/handlers/Handler.js
+++ src/handlers/Handler.js
@@ -13,49 +13,70 @@ const badTags = {
 export default class Handler {
   constructor() {
     this.stack = [];
+    this.noscript = false;
   }
-  
+
   onopentag(name, attributes) {
+    if (name === 'noscript') {
+      this.noscript = true;
+    }
+
+    if (this.noscript) {
+      return;
+    }
+
     this.stack.push({ name, attributes, text: '' });
     if (badTags[name]) {
       return;
     }
-    
+
     if (typeof this[name + 'Start'] === 'function') {
       this[name + 'Start'](attributes);
     }
-    
+
     if (typeof this.anyStart === 'function') {
       this.anyStart(name, attributes);
     }
   }
-  
+
   ontext(text) {
+    if (this.noscript) {
+      return;
+    }
+
     if (this.stack.length) {
       for (let item of this.stack) {
         item.text += text;
       }
     }
   }
-  
-  onclosetag() {
-    if (!this.stack.length) return;
+
+  onclosetag(tagName) {
+    if (tagName === 'noscript') {
+      this.noscript = false;
+      return;
+    }
+
+    if (this.noscript || !this.stack.length) {
+      return;
+    }
+
     let { name, attributes, text } = this.stack.pop();
     if (badTags[name]) {
       return;
     }
-    
+
     text = entities.decodeHTML(text);
-    
+
     if (typeof this[name] === 'function') {
       this[name](attributes, text);
     }
-    
+
     if (typeof this.any === 'function') {
       this.any(name, attributes, text);
     }
   }
-  
+
   getResult() {
     return this.result;
   }
